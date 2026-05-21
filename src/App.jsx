0164a0af908388e8bffefed10642eed434e17664
wnghdals123 <jhm9325@naver.com>
2026-05-21 13:59:24 +0900

    결제 설정 수정

--- src/App.jsx
+++ src/App.jsx
@@ -159,13 +159,13 @@ export default function App() {
 
     IMP.request_pay(
       {
-        pg: "tosspayments",
         channel_key: "channel-key-766362a2-6f8e-4d4e-9bc3-10119735dd6a",
-        pay_method: payMethod,
+        pay_method: "card",
         merchant_uid: `order_${num}_${Date.now()}`,
         name: orderItems.map((i) => i.name).join(", "),
         amount: totalPrice,
         buyer_name: "카페 손님",
+        buyer_tel: "010-0000-0000",
       },
       async (rsp) => {
         if (rsp.success) {
